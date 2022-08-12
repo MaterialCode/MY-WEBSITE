1723f2c41000da9140851d840568d097f3c6d013
Jay Pandya <91810922+CoderForLife123@users.noreply.github.com>
2022-08-12 20:03:22 +1000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(window).load(function () {
+  $("#preloader-wrapper").fadeOut("slow");
+});
+
+$(document).ready(function () {
+  //animated header class
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    //console.log(scroll);
+    if (scroll > 200) {
+      //console.log('a');
+      $(".navigation").addClass("animated");
+    } else {
+      //console.log('a');
+      $(".navigation").removeClass("animated");
+    }
+  });
+
+  $(".gallery-slider").owlCarousel({
+    pagination: true,
+    autoPlay: 5000,
+    itemsDesktop: [1500, 4],
+    itemsDesktopSmall: [979, 3],
+  });
+
+  // Gallery Popup
+  $(".image-popup").magnificPopup({ type: "image" });
+});
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
