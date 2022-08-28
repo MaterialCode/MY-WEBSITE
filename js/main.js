2b9ab85dcceaa9f0f5e0cf56059abef733735444
Jay Pandya <91810922+CoderForLife123@users.noreply.github.com>
2022-08-28 12:24:29 +1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -27,29 +27,18 @@ $(document).ready(function () {
   $(".image-popup").magnificPopup({ type: "image" });
 });
 const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
 
-hamburger.addEventListener('click', ()=>{
-   //Animate Links
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
 
-    //Hamburger Animation
-    hamburger.classList.toggle("toggle");
-});
-$(function () {
-  $(".col-md-4").slice(0, 3).show();
-  $("body").on('click touchstart', '.load-more', function (e) {
-    e.preventDefault();
-    $(".col-md-4:hidden").slice(0, 3).slideDown();
-    if ($(".col-md-4:hidden").length == 0) {
-      $(".load-more").css('visibility', 'hidden');
-    }
-    $('html,body').animate({
-      scrollTop: $(this).offset().top
-    }, 1000);
-  });
-});
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
